f67a6fde396d4117efca5e5d25d16d0464012ac0
aniruddha <aniruddha.kudalkar@gmail.com>
2020-03-07 12:30:27 +0530

    angular : content child and view child

--- angular/my-app/projects/local-refs/src/app/app.component.ts
+++ angular/my-app/projects/local-refs/src/app/app.component.ts
@@ -1,20 +1,27 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'local-refs';
 
   @ViewChild('nm')
   nm : ElementRef
+
+  @ViewChild('jumbo')
+  jmb : ElementRef
 
   consoleValue() {
     console.log(this.title)
     console.log(this.nm)
     this.nm.nativeElement.style.border = '1px solid red'
     // nm.style.border = '1px solid red'
+  }
+
+  ngAfterViewInit() {
+    //this.jmb.nativeElement.style.border = '1px solid red'
   }
 }
--- angular/my-app/projects/local-refs/src/app/sample-comp/sample-comp.component.ts
+++ angular/my-app/projects/local-refs/src/app/sample-comp/sample-comp.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-sample-comp',
   templateUrl: './sample-comp.component.html',
   styleUrls: ['./sample-comp.component.css']
 })
-export class SampleCompComponent implements OnInit {
+export class SampleCompComponent implements OnInit, AfterViewInit {
+
+  @ContentChild('jumbo')
+  jmb : ElementRef
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.jmb.nativeElement.style.border = '1px solid red'
   }
 
 }
